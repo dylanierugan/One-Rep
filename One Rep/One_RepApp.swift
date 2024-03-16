48dd5ecf1e9807c52b333d0c19eb49fa7364dbf5
Dylan Ierugan <dylanierugan@gmail.com>
2024-03-15 19:15:53 -0700

    Initial Commit

---
+++ One Rep/One_RepApp.swift
@@ -0,0 +1,17 @@
+//
+//  One_RepApp.swift
+//  One Rep
+//
+//  Created by Dylan Ierugan on 3/15/24.
+//
+
+import SwiftUI
+
+@main
+struct One_RepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
