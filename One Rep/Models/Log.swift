e4a9c7e2e0dc1f7616c1770a48aa537f2330a961
Dylan Ierugan <dylanierugan@gmail.com>
2024-04-25 23:14:39 -0700

    Log model

--- One Rep/Models/Log.swift
+++ One Rep/Models/Log.swift
@@ -8,6 +8,18 @@
 import Foundation
 import RealmSwift
 
-class Log: Object {
+class Log: Object, ObjectKeyIdentifiable{
     @Persisted(primaryKey: true) var id: ObjectId
+    @Persisted var reps: Int
+    @Persisted var weight: Double
+    @Persisted var isBodyWeight: Bool
+    @Persisted var repType: String
+    
+    convenience init(reps: Int = 0, weight: Double = 0, isBodyWeight: Bool, repType: String) {
+        self.init()
+        self.reps = reps
+        self.weight = weight
+        self.isBodyWeight = isBodyWeight
+        self.repType = repType
+    }
 }
